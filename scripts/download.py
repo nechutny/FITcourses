7cfc33a8447e77490dd0d4cd2ecc6393e838ef1c
Vojta Mrazek <mrazek.v@gmail.com>
2013-01-02 20:46:15 +0100

    Presun do skripts

--- scripts/download.py
+++ scripts/download.py
@@ -6,23 +6,21 @@ import urllib2
 import re
 import os
 
-path="https://wis.fit.vutbr.cz/FIT/st/course-g.php?ects=1&id="
+path = "https://wis.fit.vutbr.cz/FIT/st/course-g.php?ects=1&id="
 
 def downloadYear(year):
     """ Stazeni celeho roku """
-    path="http://www.fit.vutbr.cz/study/course-l.php.cs?rok=" + year
-    f=urllib.urlopen(path)
-    list=f.read().split("\n") #replace("\n","").split("tr")
-    courses=[]
+    path = "http://www.fit.vutbr.cz/study/course-l.php.cs?rok=" + year
+    f = urllib.urlopen(path)
+    list = f.read().split("\n") #replace("\n","").split("tr")
+    courses = []
     # nalezeni seznamu predmetu
     for line in list:
-        #m=re.search(r"<a href=\"[^\"0-9]*([0-9]+)\".*<b>([A-Z]+[0-9]*)</b>",line, re.DOTALL)
-        m=re.search(r"<a\s*href=\"[^\"0-9]*([0-9]+)\".*<b>(.*)</b>",line, re.DOTALL)
+        m = re.search(r"<a\s*href=\"[^\"0-9]*([0-9]+)\".*<b>(.*)</b>",line, re.DOTALL)
         if m:
             courses += [(year, m.group(1),m.group(2))]
         else:
             pass
-#            print line
     return courses
 
 def download(url, local):
@@ -44,13 +42,12 @@ except:
     pass
 
 
-courses=[]
+courses = []
+courses += downloadYear(year)
 
-courses+=downloadYear(year)
-
-coursesList=dict()
+coursesList = dict()
 # Nalezeni vsech predmetu
-for y,i,c in courses:
+for y, i, c in courses:
     print "Stahuji predmet %s" % c
     download(path + i, "%s/%s.png" % (y, c))
     
