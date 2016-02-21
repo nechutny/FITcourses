e6ea44e2b1c86284a202cc163cbc4fba5c61d170
Stanislav Nechutný <stanislav@nechutny.net>
2016-02-21 12:14:21 +0100

    Doplneno logo fitu

--- template/homepage.tpl.php
+++ template/homepage.tpl.php
@@ -1,4 +1,6 @@
           <div class="jumbotron">
+			  <img src="img/FIT_zkratka_barevne_RGB_CZ.png" align="left" width="300" style="margin: 30px;">
             <h1>FIT Courses</h1>
+
             <p>Přehled úspěšnosti v předmětech na FIT VUT v Brně. Pro zobrazení statistik je nutné být přihlášený do <a href="http://wis.fit.vutbr.cz" target="_blank">WISu <span class="glyphicon glyphicon-new-window"></span></a>.</p>
           </div>
